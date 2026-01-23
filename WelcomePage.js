c60cecb3453212e4e15288a39fea8dd8c8229929
mandalapudisusanna <mandalapudisusanna@gmail.com>
2026-01-23 12:55:31 +0530

    Add files via upload

---
+++ WelcomePage.js
@@ -0,0 +1,14 @@
+import React,{useContext}from 'react';
+import { UserContext } from './UserContext';
+
+const WelcomePage=()=>{
+    const {user}= useContext(UserContext);
+
+    return(
+        <div>
+            <h1>Welcome User:</h1>
+            <p>Name: {user.name} id:{user.id}</p>
+        </div>
+    );
+};
+export default WelcomePage;
